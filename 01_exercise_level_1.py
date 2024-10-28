2a09635a6e11d8292b629fd58fdc3a63ad5b78fc
myeraug <140523979+myeraug@users.noreply.github.com>
2024-10-28 20:52:14 +0800

    Add files via upload

---
+++ 01_exercise_level_1.py
@@ -0,0 +1,46 @@
+# Check the python version you are using
+# by using cmd
+# Python version 3.12.1
+
+# Open the python interactive shell and do the following operations. The operands are 3 and 4.
+# addition
+print(3 + 4)
+
+# subtraction
+print(3 - 4)
+
+# multiplication
+print(3 * 4)
+
+# modulus
+print(3 % 4)
+
+#division
+print(3 / 4)
+
+# exponential
+print(3 ** 4)
+
+# floor division operator
+print(3 // 4)
+
+# Write strings on the python interactive shell. The strings are the following:
+# your name
+print("Anakin Skywalker")
+
+# your family name
+print("The Skywalker Family")
+
+# your country
+print("The United States of America")
+
+print("I am enjoying 30 days of python")
+
+# Check the data types of the following data
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(["Asabeneh", "Python", "Finland"]))
+print(type("Your name"))
+print(type("Your country"))
